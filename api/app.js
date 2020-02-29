cceee82478ee4ae4459387bf19a6ab50e895a93b
thook07 <thook07@gmail.com>
2020-02-29 16:19:20 -0700

    updating the api

--- api/app.js
+++ api/app.js
@@ -2,6 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => 
+    res.send('Hello World!');
+)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/recipes/' function(req, res) {
+    
+    res.statusCode = 200;
+    res.json([{
+            'id': 'chocolate-chip-cookies-2',
+            'name': 'Chocolate Chip Cookies',
+            'tags': [{
+                'id': 'cookie',
+                'name': 'Cookie'
+            },
+            {
+                'id': 'dessert',
+                'name': 'Dessert'
+            }],
+            'id': 'jalapeno-mac-and-cheese',
+            'name': 'Jalapeno-mac-and-cheese',
+            'tags': [{
+                'id': 'cookie',
+                'id': 'dessert'
+            }]
+        }]
+    });
+    return res;
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
