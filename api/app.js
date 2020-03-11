4b7ac4871ded9cf5067abaa70cd111ae26cce4c0
thook07 <thook07@gmail.com>
2020-03-11 09:33:03 -0700

    test

--- api/app.js
+++ api/app.js
@@ -68,6 +68,11 @@ router.use(function(req, res, next) {
 **********************************************/
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //this API is used to authenticate a user and then send back a Auth Token.
