b426dcda18fc4188b4942675dbe3e93feda2ffbb
thook07 <thook07@gmail.com>
2020-03-10 21:02:06 -0700

    test

--- api/import.js
+++ api/import.js
@@ -93,6 +93,55 @@ function storeTags(recipes) {
         }
     }
     console.log(tags);
+    for(var i=0; i<tags.length; i++){
+        storeTag(tags[i]);
+    }
+    
+    
+}
+
+function storeTag(tag){
+    log.debug("tag [" + tag + "]");
+    var query = `
+        INSERT INTO tags (
+            id
+        )
+        VALUES (
+            ?
+        );
+    `
+    var values = [
+        tag
+    ]
+    
+    log.trace("query[" + query + "]");
+    mysql.con.query(query, values, function(err,rows){
+        if(err) { 
+            log.error("Error occurred while grabing order archive information.");
+            log.error("Error Msg: " + err);
+            throw err;
+        } else {
+            log.debug("Success. ["+recipe.name+"] was written to the datbase")
+        }
+    });
+    
+}
+
+function storeInstructions(recipes) {
+    console.log("Recipes: ["+recipes.length+"]")
+    var tags = [];
+    for(var i=0; i < recipes.length; i++){
+        console.log("Recipe: ["+recipes[i].name+"]");
+        console.log("Recipe Tags: ["+recipes[i].tags+"]");
+        for(var j=0; j < recipes[i].tags.length; j++) {
+            var tag = recipes[i].tags[j]
+            console.log("Tag ["+tag+"]");
+            if(tags.indexOf(tag) == -1) {
+                tags.push(tag);
+            }
+        }
+    }
+    console.log(tags);
     
 }
 
